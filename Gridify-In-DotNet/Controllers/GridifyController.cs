f0eb021c5033e81e91eb357bd137502f926d7293
MgKyaw <mgkyaw20112@gmail.com>
2025-08-26 19:29:35 +0700

    feat: Add GridifyController

---
+++ Gridify-In-DotNet/Controllers/GridifyController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gridify_In_DotNet.Controllers;
+public class GridifyController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
