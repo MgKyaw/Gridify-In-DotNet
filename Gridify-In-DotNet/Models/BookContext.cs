4ebdb152d9c6872412b6c9ed07d9ac350eb38a93
MgKyaw <mgkyaw20112@gmail.com>
2025-08-26 19:16:14 +0700

    fix: Rename - from namespace

--- Gridify-In-DotNet/Models/BookContext.cs
+++ Gridify-In-DotNet/Models/BookContext.cs
@@ -1,21 +1,22 @@
-namespace Gridify-In-DotNet.Models;
-
-public class BookContext : DbContext
+namespace GridifyInDotNet.Models
 {
-    protected BookContext(DbContextOptions options)
-        : base(options) { }
-
-    public BookContext(DbContextOptions<BookContext> options)
-        : this((DbContextOptions)options)
+    public class BookContext : DbContext
     {
+        protected BookContext(DbContextOptions options)
+            : base(options) { }
+
+        public BookContext(DbContextOptions<BookContext> options)
+            : this((DbContextOptions)options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
     }
 
-    public DbSet<Book> Books { get; set; }
-}
-
-public class Book
-{
-    public Guid BookId { get; set; } = Guid.NewGuid();
-    public string Title { get; set; }
-    public DateTime Date { get; set; }
+    public class Book
+    {
+        public Guid BookId { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
 }
