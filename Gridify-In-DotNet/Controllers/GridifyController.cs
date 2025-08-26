07a355788950b579fda0c23beeb5156e2bd926e3
MgKyaw <mgkyaw20112@gmail.com>
2025-08-26 19:32:59 +0700

    feat: Add GetBooks api

--- Gridify-In-DotNet/Controllers/GridifyController.cs
+++ Gridify-In-DotNet/Controllers/GridifyController.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gridify;
+using GridifyInDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gridify_In_DotNet.Controllers;
-public class GridifyController : Controller
+
+[ApiController]
+[Route("[controller]")]
+public class GridifyController : ControllerBase
 {
-    public IActionResult Index()
+    private readonly BookContext _context;
+
+    public GridifyController(BookContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    [HttpGet("books")]
+    public IActionResult GetBooks([FromQuery] GridifyQuery model)
+    {
+        if (!_context.Books.Any())
+        {
+            var books = Enumerable.Range(1, 100).Select(index => new Book
+            {
+                Title = $"Book {index}",
+                Date = DateTime.Now.AddDays(-index)
+            }).ToList();
+
+            _context.Books.AddRange(books);
+            _context.SaveChanges();
+        }
+
+        var result = _context.Books.GridifyQueryable(model);
+
+        return Ok(result);
     }
 }
